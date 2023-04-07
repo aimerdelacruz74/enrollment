084b33be889d241c9d6994361ee6d179761f1534
aimerdelacruz74 <80333393+aimerdelacruz74@users.noreply.github.com>
2023-04-08 00:17:32 +0800

    Add files via upload

---
+++ privacy.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+
+def show_privacy():
+
+
+    streamlit_style = """
+        <style>
+        @import url('https://fonts.googleapis.com/css?family=Poppins');
+
+        html, body, [class*="css"]  {
+        font-family: 'Poppins', sans-serif;
+        }
+        </style>
+        """
+    st.markdown(streamlit_style, unsafe_allow_html=True)
+
+    st.title("PRIVACY CONSENT STATEMENT")
+    st.write("")
+    st.write("Holy Angel University respects the student’s privacy and are committed to protecting information. By using the enrollment prediction app, applicants are consenting to the collection, use, and disclosure of personal information as described in this privacy policy.")
+    st.write("")
